1bc857cfd008b0a7721bfebf9f66a47f6db32297
fangkun <fangkun@ideamake.cn>
2022-10-09 06:29:38 +0800

    feat: add markdown hightlight

--- src/components/MarkdownEditor/index.tsx
+++ src/components/MarkdownEditor/index.tsx
@@ -2,6 +2,9 @@ import { marked } from "marked";
 import { useLocalStorage } from "../../hooks/useLocalstorage";
 import Editor from "../Editor/Editor";
 import "./index.css"
+import hljs from "highlight.js";
+import 'highlight.js/styles/monokai.css';
+import { onMount } from "solid-js";
 
 const INIT_VALUE = `# Marked in the browser\n\nRendered by **marked**.`;
 
@@ -9,17 +12,25 @@ const INIT_VALUE = `# Marked in the browser\n\nRendered by **marked**.`;
 export default function MarkdownEditor() {
   const [value, setValue] =  useLocalStorage('markdown', INIT_VALUE)
   const html = () => marked(value());
-
+  let previewRef: HTMLDivElement| undefined
   const valueChangeHandle = (v: string) => {
     setValue(v)
+    previewRef!.querySelectorAll('pre > code').forEach(codeItem => {
+      // const lang = codeItem.classList.value
+      hljs.highlightElement(codeItem as HTMLElement)
+    })
   }
+  onMount(() => {
+    valueChangeHandle(value)
+  })
+
 
   return (
     <div class="h-[100vh] flex">
       <div class="flex-1">
         <Editor language="markdown" value={value()} onChange={valueChangeHandle} />
       </div>
-      <div  class="flex-1 markdown-body pl-4" innerHTML={html()}></div>
+      <div  class="flex-1 markdown-body pl-4" ref={previewRef} innerHTML={html()}></div>
     </div>
   );
 }
